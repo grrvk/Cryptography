edfd7832af70f93e0144521e1f722be5f51712d0
grrvk <112933732+grrvk@users.noreply.github.com>
2024-05-13 13:58:49 +0300

    L2

---
+++ CryptLab2/README.md
@@ -0,0 +1,20 @@
+#  Lab2
+
+Algorythm encode/decode working time depending on binary length:
+
+|   Bits    |  Encode    |  Decode    |
+|-----------|------------|------------|
+|    2^6    | 0.080834ms.| 0.023875ms.|
+|    2^9    | 0.13675ms. | 0.020916ms.|
+|    2^12   | 0.612334ms.| 0.032583ms.|
+|    2^16   | 11.1118ms. | 0.023542ms.| 
+
+Algorythm total working time depending on binary length:
+
+|   Bits    |  Total      |
+|-----------|-------------|
+|    2^6    | 118792ms.   |
+|    2^9    | 183542ms.   |
+|    2^12   | 840417ms.   |
+|    2^16   | 109610333ms.| 
+
---
+++ CryptLab2/scheme.h
@@ -0,0 +1,93 @@
+#ifndef scheme_h
+#define scheme_h
+#include "functions.h"
+
+using namespace std;
+
+
+class RSA{
+    __int128_t p;
+    __int128_t q;
+    tuple<__int128_t, __int128_t> publicKey;
+    tuple<__int128_t, __int128_t> privateKey;
+public:
+    void generateKeys(__int128_t bitNums){
+        vector<__int128_t> primes = generateBitPrimes(bitNums, 2);
+        p = primes[0]; q = primes[1];
+        __int128_t n = p * q;
+        
+        __int128_t phi_n = Carmichael_function(p-1,q-1);
+        __int128_t e = generate_coprime(phi_n);
+        __int128_t d = extended_euclid(e, phi_n);
+
+        if ((e == 0) || (phi_n == 0)){
+            generateKeys(bitNums);
+        }
+        else{
+            this->publicKey = tuple<__int128_t, __int128_t> (e, n);
+            this->privateKey = tuple<__int128_t, __int128_t> (d, n);
+        }
+    }
+    
+    vector<__int128_t> encode_message(vector<__int128_t> m, tuple<__int128_t, __int128_t> key){
+        for (int i = 0; i < size(m); i++){
+            m[i] = powerMod((int)m[i], get<0>(key), get<1>(key));
+        }
+        cout << endl << "Encrypted message:" << endl;
+        printVector(m); cout << endl;
+        return m;
+    }
+    
+    vector<__int128_t> decode_message(vector<__int128_t> c, tuple<__int128_t, __int128_t> key){
+        for (int i = 0; i < size(c); i++){
+            c[i] = chineseRemainder((int)c[i], get<0>(key), p, q);
+        }
+        //printVector(c);
+        return c;
+    }
+    
+    tuple<__int128_t, __int128_t> getPublicKey(){
+        return publicKey;
+    }
+    
+    tuple<__int128_t, __int128_t> getPrivateKey(){
+        return privateKey;
+    }
+};
+
+class Person{
+    string name;
+    RSA rsa;
+    tuple<__int128_t, __int128_t> privateKey;
+    tuple<__int128_t, __int128_t> publicKey;
+public:
+    Person(string name){
+        this->name = name;
+    }
+    
+    void setPublicKey(tuple<__int128_t, __int128_t> key){
+        this->publicKey = key;
+    }
+    
+    tuple<__int128_t, __int128_t> generateKeys(__int128_t bitsNum){
+        this->rsa = RSA();
+        rsa.generateKeys(bitsNum);
+        this->privateKey = rsa.getPrivateKey();
+        return rsa.getPublicKey();
+    }
+    
+    vector<__int128_t> sendMessage(string m){
+        vector<__int128_t> numericMessage = getNumMessage(m);
+        //printVector(numericMessage);
+        vector<__int128_t> encodedMessage = rsa.encode_message(numericMessage, publicKey);
+        return encodedMessage;
+    }
+    
+    string recieveMessage(vector<__int128_t> c){
+        vector<__int128_t> numericMessage = rsa.decode_message(c, privateKey);
+        string message = getStrMessage(numericMessage);
+        return message;
+    }
+};
+
+#endif /* scheme_h */
